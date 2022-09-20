5aa7258119fb6dfc2784eb06ddd18f1e2d52b443
Leo Fox <leofoxyoutb@gmail.com>
2022-09-20 22:51:07 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ⌨️ Viertual Mechanical Keyboard
+
+This is a simulator program, it recreates mechanical keyboard sound effects with python.
+
+## How to run this crap?
+```python
+# First you have to install the dependencies
+$ pip install -r requirements.txt
+
+# And then run it 
+
+# click mode
+$ python main.py click
+
+# smash mode
+$ python main.py smash
+```
---
+++ main.py
@@ -0,0 +1,16 @@
+from pynput.keyboard import Listener
+from playsound import playsound
+from random import choice
+import sys
+
+def on_press(key):
+    if sys.argv[1] == 'click':
+        random_choice = choice(['1', '2', '3', '4', '5', '6'])
+    elif sys.argv[1] == 'smash':
+        random_choice = choice(['1', '2', '3', '4'])
+
+    playsound(f"./{sys.argv[1]}{random_choice}.wav", False)
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
